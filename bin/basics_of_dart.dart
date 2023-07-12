17cd435f4f6b108f62754e2e7bb8e74bf5a43d75
tynchtykestebes <tynchtyk.estebesuulu.bj@gmail.com>
2023-07-12 20:01:30 +0600

    variables

--- bin/basics_of_dart.dart
+++ bin/basics_of_dart.dart
@@ -61,7 +61,7 @@ void main() {
   print("7________________________________________________");
   int myThirdValue = 25;
   print(myThirdValue);
-  myThirdValue -= 75;
+  myThirdValue -= 75; // myThirdValue = myThirdValue - 75
   print(myThirdValue);
 
   print("8________________________________________________");
@@ -87,4 +87,26 @@ void main() {
   print(greeting4);
   greeting4 = 'Hello \nTynchtyk \nHow are you'; // \n is for new line
   print(greeting4);
+
+  print(
+      "12. var, final and const________________________________________________");
+  // var/final/const variableName = value
+  // we CAN'T start variable name with numbers
+  var someValue1 = '10'; // var is mutable
+  final someValue2 = '10'; // final can only be set once, final is immutable
+  const someValue3 = '10'; // const can only be set onse, const is immutable
+
+  print(someValue1);
+  print(someValue2);
+  print(someValue3);
+  print("Reassigning");
+  someValue1 = '10001'; // var can be reassigned, but final and const can't
+  print(someValue1);
+
+  print("13. final and const________________________________________________");
+  final someValue22 = DateTime.now();
+  const someValue33 = 'runtime variables cant be const';
+  // DateTime.now() doesn't work with const, because current time is always changing
+  print(someValue22);
+  print(someValue33);
 }
